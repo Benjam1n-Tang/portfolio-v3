94c61242f59fa7946f5be58c6a6bb18839c2f9ee
Benjamin Tang Desktop <benjamin.code.tang@gmail.com>
2026-01-11 00:54:09 -0500

    Add graphik font

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -6,13 +6,9 @@ import VideoBackground from "@/components/backgrounds/VideoBackground";
 export default function Home() {
   return (
     <main className="relative h-[100dvh] w-screen overflow-hidden">
-      <InteractiveDots />
-
-      <h1
-        className="absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 z-10 text-white text-[34px] text-center"
-        style={{ mixBlendMode: "difference" }}
-      >
-        Next.js background video
+      <VideoBackground />
+      <h1 className="absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 z-10 text-white text-8xl text-center mix-blend-difference font-semibold font-graphik">
+        BENJAMIN TANG
       </h1>
     </main>
   );
