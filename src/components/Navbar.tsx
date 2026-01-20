a6e9dc21531b007a438219bd479958c5d7e83c4c
Benjamin Tang Desktop <benjamin.code.tang@gmail.com>
2026-01-19 21:23:31 -0500

    Further fix lando text effect

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -3,23 +3,6 @@ import Link from "next/link";
 const SwapText: React.FC<{ text: string }> = ({ text }) => {
   const chars = Array.from(text);
 
-  const easeForIndex = (i: number, total: number) => {
-    const t = i / Math.max(total - 1, 1);
-
-    // interpolate between two premium easing curves
-    const start = [0.19, 1, 0.22, 1]; // heavier
-    const end = [0.16, 1.2, 0.24, 1]; // lighter
-
-    const lerp = (a: number, b: number) => a + (b - a) * t;
-
-    return `cubic-bezier(
-    ${lerp(start[0], end[0])},
-    ${lerp(start[1], end[1])},
-    ${lerp(start[2], end[2])},
-    ${lerp(start[3], end[3])}
-  )`;
-  };
-  
   return (
     <span className="text-white font-neue-montreal uppercase font-medium relative block text-lg overflow-hidden whitespace-nowrap leading-[0.9]">
       {/* top */}
@@ -27,35 +10,37 @@ const SwapText: React.FC<{ text: string }> = ({ text }) => {
         {chars.map((char, i) => (
           <span
             key={`top-${i}`}
-            className="inline-block will-change-transform transition-transform group-hover:-translate-y-[95%]"
+            className="inline-block will-change-transform transition-transform group-hover:-translate-y-[110%]"
             style={{
-              transitionDuration: "600ms",
-              transitionTimingFunction: easeForIndex(i, chars.length),
+              transitionDuration: "250ms",
+              transitionDelay: `${i * 25}ms`,
+              transitionTimingFunction: "ease-in-out",
             }}
           >
-            {char}
+            {char === " " ? "\u00A0" : char}
           </span>
         ))}
       </span>
-
       {/* bottom */}
       <span className="absolute inset-0">
         {chars.map((char, i) => (
           <span
             key={`bottom-${i}`}
-            className="inline-block will-change-transform transition-transform translate-y-[95%] group-hover:translate-y-0"
+            className="inline-block will-change-transform transition-transform translate-y-[110%] group-hover:translate-y-0"
             style={{
-              transitionDuration: "600ms",
-              transitionTimingFunction: easeForIndex(i, chars.length),
+              transitionDuration: "250ms",
+              transitionDelay: `${i * 25}ms`,
+              transitionTimingFunction: "ease-in-out",
             }}
           >
-            {char}
+            {char === " " ? "\u00A0" : char}
           </span>
         ))}
       </span>
     </span>
   );
 };
+
 const Navbar = () => {
   const menuItems = [
     { label: "Home", link: "/" },
@@ -73,7 +58,7 @@ const Navbar = () => {
             <Link
               key={item.label}
               href={item.link}
-              className="group text-lg hover:opacity-70 transition-opacity duration-300"
+              className="group text-lg transition-colors duration-200"
             >
               <SwapText text={item.label} />
             </Link>
