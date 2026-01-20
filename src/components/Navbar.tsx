1cd40b0973f64005609572168e217f3a3961e225
Benjamin Tang Desktop <benjamin.code.tang@gmail.com>
2026-01-19 21:14:59 -0500

    Add nav link text effect

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,6 +1,44 @@
 import Link from "next/link";
-import Shuffle from "./Shuffle";
 
+const SwapText: React.FC<{ text: string }> = ({ text }) => {
+  return (
+    <span className="text-white font-neue-montreal uppercase font-medium relative block text-lg overflow-hidden whitespace-nowrap leading-[0.9]">
+      {/* top */}
+      <span className="block">
+        {Array.from(text).map((char, i) => (
+          <span
+            key={`top-${i}`}
+            className="inline-block will-change-transform will-change-opacitytransition-transform group-hover:-translate-y-[95%]"
+            style={{
+              transitionDuration: "600ms",
+              transitionTimingFunction: "cubic-bezier(0.19, 1, 0.22, 1)",
+              transitionDelay: `${i * 15}ms`,
+            }}
+          >
+            {char}
+          </span>
+        ))}
+      </span>
+
+      {/* bottom */}
+      <span className="absolute inset-0">
+        {Array.from(text).map((char, i) => (
+          <span
+            key={`bottom-${i}`}
+            className="inline-block will-change-transform transition-transform translate-y-[95%] group-hover:translate-y-0"
+            style={{
+              transitionDuration: "600ms",
+              transitionTimingFunction: "cubic-bezier(0.19, 1, 0.22, 1)",
+              transitionDelay: `${i * 15}ms`,
+            }}
+          >
+            {char}
+          </span>
+        ))}
+      </span>
+    </span>
+  );
+};
 
 const Navbar = () => {
   const menuItems = [
@@ -14,12 +52,14 @@ const Navbar = () => {
     <nav className="w-full py-[3rem] md:px-[2.5rem] px-5 flex justify-between items-center">
       <h1> LOGO HERE </h1>
       <div>
-        <div className="flex flex-row gap-8">
+        <div className="flex flex-row gap-[2.5rem]">
           {menuItems.slice(1).map((item) => (
-            <Link key={item.label} href={item.link}>
-              <label className="font-medium cursor-pointer">
-                {item.label}
-              </label>
+            <Link
+              key={item.label}
+              href={item.link}
+              className="group text-lg hover:opacity-70 transition-opacity duration-300"
+            >
+              <SwapText text={item.label} />
             </Link>
           ))}
         </div>
